9f2e39edd0fbea77cef8bbf3653dce57af26c284
Gerardo Llanos <gerardollanos@protonmail.com>
2024-08-05 17:35:13 -0400

    cors update

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -6,6 +6,8 @@ const allowedOrigins = [
   "https://www.latebanbarber.com",
   "https://tebanbarber-gitpage.vercel.app.com",
   "https://www.tebanbarber-gitpage.vercel.app.com",
+  "https://tebanbarber-gitpage.vercel.app/",
+  "https:///www.tebanbarber-gitpage.vercel.app/",
 ];
 export const corsOptions = {
   origin: allowedOrigins,
